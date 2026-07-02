43d07af2e534a669d02bbf3f64fb194fab375218
appolosx6-gif <vinorajeg8@gmail.com>
2026-07-02 09:59:58 +0700

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,29 @@
+module.exports = (req, res) => {
+  const data = {
+    DPMods_Security: {
+      App_Status: {
+        Admin_URL: "https://t.me/kepental",
+        Banned_Devices: "hacker_hwid_001,hacker_hwid_002",
+        Dialog_Subtitle: "Secure authentication required.",
+        Dialog_Title: "SYSTEM ACCESS",
+        Maintenance: false,
+        Update_Link: "https://t.me/AppoloSX6_Channel",
+        Update_Required: false
+      },
+      Keys: {
+        PinokCrack: {
+          Banned: false,
+          DeviceLimit: 9999,
+          Devices: {
+            "dummy": 0
+          },
+          ExpiryDate: "2029-12-31",
+          Username: "PinokCrack"
+        }
+      }
+    }
+  };
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json(data);
+};
