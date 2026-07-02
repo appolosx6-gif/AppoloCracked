b71493eb2967fb44a2bad3cc61cb7b679aea03fa
appolosx6-gif <vinorajeg8@gmail.com>
2026-07-02 16:45:34 +0700

    Add files via upload

---
+++ databases/keys.js
@@ -0,0 +1,14 @@
+export const KEYS = [
+  {
+    key: "PinoCrack",
+    games: ["FREEFIRE", "MLBB"],
+    expired: "2099-12-31",
+    banned: false
+  },
+  {
+    key: "PinoCrackVIP",
+    games: ["FREEFIRE"],
+    expired: "2099-12-31",
+    banned: false
+  }
+];
